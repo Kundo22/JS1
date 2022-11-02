f89b7dcfb3ab3d71d2e87f0b322d88dccc5a8c57
Kundo22 <107933722+Kundo22@users.noreply.github.com>
2022-11-02 19:15:05 -0300

    subida1

---
+++ stock.js
@@ -0,0 +1,8 @@
+let stockProductos = [
+    {id: 1, nombre: "Cafe", tipo: "cafe", cantidad:1,  precio: 300,  img: './img/menu-1.jpg'},
+    {id: 2, nombre: "Cafe con leche", tipo: "cafe", cantidad:1, precio: 320,  img: './img/menu-2.jpg'},
+    {id: 3, nombre: "Cafe con cookies", tipo: "cafe", cantidad:1, precio: 420, img: './img/menu-3.jpg'},
+    {id: 4, nombre: "Cafe con medialunas", tipo: "cafe", cantidad:1, precio: 400, img: './img/menu4.jpg'},
+   
+    
+]
